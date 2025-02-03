a308fbcf42e0076997c2e110019dab25875af057
FloresA06 <agflores616@gmail.com>
2025-02-03 15:52:07 -0600

    Delete Semana1 directory

--- Semana1/Prob6.py
+++
@@ -1,9 +0,0 @@
-#Este programa convierte grados Celsius a Fahrenheit y Kelvin
-
-temp_cel=float(input("Ingresar temperatura en Celsius: "))
-
-temp_far=temp_cel*9/5+32
-temp_kel=temp_cel+273.15
-
-print("La temperatura en grados Fahrenheit es: ",temp_far)
-print("La temperatura en grados Kelvin es: ", temp_kel)
