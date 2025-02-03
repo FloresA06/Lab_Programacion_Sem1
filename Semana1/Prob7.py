04fa593329e1cf28d3a2727e3029d6d3d4ecfa01
FloresA06 <agflores616@gmail.com>
2025-02-03 15:44:22 -0600

    Delete Semana1 directory

--- Semana1/Prob7.py
+++
@@ -1,8 +0,0 @@
-#Este programa calcula el IMC
-
-peso=float(input("Ingresar su peso en kilogramos: "))
-alt=float(input("Ingresar su altura en metros: "))
-
-imc=peso/alt**2
-
-print("Su indice de masa corporal (IMC) es: ", imc)
